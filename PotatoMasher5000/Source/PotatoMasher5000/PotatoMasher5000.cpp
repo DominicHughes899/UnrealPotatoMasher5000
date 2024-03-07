ff7dfb474400d41e92aa092eb646e56e9066817c
DominicHughes899 <91923512+DominicHughes899@users.noreply.github.com>
2024-03-07 11:54:30 +0000

    initial commit: empty unreal project

---
+++ PotatoMasher5000/Source/PotatoMasher5000/PotatoMasher5000.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PotatoMasher5000.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PotatoMasher5000, "PotatoMasher5000" );
---
+++ PotatoMasher5000/Source/PotatoMasher5000/PotatoMasher5000GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PotatoMasher5000GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POTATOMASHER5000_API APotatoMasher5000GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
